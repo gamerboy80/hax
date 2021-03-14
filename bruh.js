8ff92836aa531abb907217d75d102f304df05ddc
gamerboy80 <68038215+gamerboy80@users.noreply.github.com>
2021-03-14 00:59:44 -0800

    Update and rename bruj.js to bruh.js

--- bruh.js
+++ bruh.js
@@ -1,5 +1,5 @@
 fetch("https://cdnjs.cloudflare.com/ajax/libs/pako/2.0.3/pako.min.js").then(resp => resp.text()).then(eval); // load pako with hax
-let in = "hello this is a test string", out;
+let in = "hello this is a test string";
 const encoder = new TextEncoder();
 const deflate = new pako.Deflate();
 let array = encoder.encode(in);
