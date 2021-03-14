6abd54e08795842c8bc9e678fc7dc9070f837e80
gamerboy80 <68038215+gamerboy80@users.noreply.github.com>
2021-03-14 00:57:44 -0800

    Update bruj.js
    editing in browser lol

--- bruj.js
+++ bruj.js
@@ -1,1 +1,8 @@
-
+fetch("https://cdnjs.cloudflare.com/ajax/libs/pako/2.0.3/pako.min.js").then(resp => resp.text()).then(eval); // load pako with hax
+let in = "hello this is a test string", out;
+const encoder = new TextEncoder();
+const deflate = new pako.Deflate();
+let array = encoder.encode(in);
+deflate.push(array, true);
+if (deflate.err) throw deflate.err;
+console.log(btoa(String.fromCharCode.apply(null, deflate.result)));
