a7817aaa5bda14bd68dfaa7fc2879407164f5736
gamerboy80 <68038215+gamerboy80@users.noreply.github.com>
2021-03-14 01:03:14 -0800

    Update bruh.js

--- bruh.js
+++ bruh.js
@@ -1,8 +1,10 @@
-fetch("https://cdnjs.cloudflare.com/ajax/libs/pako/2.0.3/pako.min.js").then(resp => resp.text()).then(eval); // load pako with hax
-let input = "hello this is a test string";
-const encoder = new TextEncoder();
-const deflate = new pako.Deflate();
-let array = encoder.encode(input);
-deflate.push(array, true);
-if (deflate.err) throw deflate.err;
-console.log(btoa(String.fromCharCode.apply(null, deflate.result)));
+(async () => {
+	await fetch("https://cdnjs.cloudflare.com/ajax/libs/pako/2.0.3/pako.min.js").then(resp => resp.text()).then(eval);
+	let input = "hello this is a test string";
+	const encoder = new TextEncoder();
+	const deflate = new pako.Deflate();
+	let array = encoder.encode(input);
+	deflate.push(array, true);
+	if (deflate.err) throw deflate.err;
+	console.log(btoa(String.fromCharCode.apply(null, deflate.result)));
+})(); // what
